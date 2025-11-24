c48d367c271dbee6412b41dffa6c2452f788528e
roimc66-create <roimc66@gmail.com>
2025-11-24 19:43:26 -0300

    new file:   Connections/conn_produtos.php

---
+++ Connections/conn_produtos.php
@@ -0,0 +1,17 @@
+<?php 
+   /// definindo variaveis para conexão   
+      $hostname_conn = "localhost";
+      $database_conn = "vpstreet_ti19";
+      $username_conn = "adminVP";
+      $password_conn = "senacvp_ti19";
+      $charset_conn = "utf8";
+      
+      $conn_produtos =   new mysqli($hostname_conn,$username_conn,$password_conn,$database_conn);
+    // Definir conjunto de caracteres da conexao
+      mysqli_set_charset($conn_produtos,$charset_conn);            
+    // verificando possiveis erros na conexao
+    if($conn_produtos->connect_error){
+        echo "Erro: ".$conn_produtos->connect_error;
+    };     
+    //espaços vazios no php causam erros Header
+?>  
