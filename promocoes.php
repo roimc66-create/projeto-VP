e6f46d5c99d656b36c3240760db75cc04a589bde
roimc66-create <roimc66@gmail.com>
2025-12-03 21:49:39 -0300

    modified:   promocoes.php

--- promocoes.php
+++ promocoes.php
@@ -20,89 +20,77 @@ $totalRows  =   ($lista_promoçao)->num_rows;
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modelo</title>
-    <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-            crossorigin="anonymous"/>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Modelo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 </head>
+
 <body>
-<div class="container-md bg-dark py-4" id="Promoçoes">
+
+ <div class="container-md bg-dark py-4" id="Promoçoes">
   <div class="d-flex">
     <div class="me-3 d-flex align-items-center">
-      <div class=" fw-bold text-uppercase" 
+      <div class="fw-bold text-uppercase"
            style="writing-mode: vertical-rl; transform: rotate(180deg); letter-spacing: 2px;">
-       <h3 class="text-bg-dark">Promoções</h3> 
+        <h3 class="text-bg-dark">Promoções</h3>
       </div>
     </div>
 
     <div class="w-100">
-      <div class="row">    
+      <div class="row">
+
+        <?php 
+        // ----- 1º card: grande -----
+        if($row = $lista_promoçao->fetch_assoc()) {
+        ?>
         <div class="col-md-6 mb-3 d-none d-sm-block d-lg-block">
-          <div class="card text-center ">
-             <a   
-           href="Produto_detalhe.php?id_produto=<?php echo $row_promoçao['id_produto']; ?>"  
-        >
-            <img src="imagens/exclusivo/<?php echo $row_promoçao['imagem_produto']; ?>" class="card-img-top img-fluid" alt="Promoção grande" >
-             <a
-                         href="Produto_detalhe.php?id_produto=<?php echo $row_promoçao['id_produto']; ?>">                       
-                    </a>  
-            <h6 class="card-title mb-1"><?php echo $row_promoçao['nome_produto'];  ?></h6>
-          <p class="card-text fw-bold"> <?php echo $row_promoçao['valor_produto'];  ?></p>  
-                                              
+          <div class="card text-center">
+            <a href="Produto_detalhe.php?id_produto=<?= $row['id_produto']; ?>">
+              <img src="imagens/exclusivo/<?= $row['imagem_produto']; ?>" 
+                   class="card-img-top img-fluid">
+            </a>
+            <h6 class="card-title mb-1"><?= $row['nome_produto']; ?></h6>
+            <p class="card-text fw-bold">
+              <?= number_format($row['valor_produto'],2,',','.'); ?>
+            </p>
           </div>
         </div>
+        <?php } ?>
 
-        <?php $i = 1;      
-        do{ $i++; ?>
-             <!-- card da esquerda -->
-        <div class="col-6 col-md-3 mb-3 ">
-          <div class="card text-center mb-3">
-            <a   
-           href="Produto_detalhe.php?id_produto=<?php echo $row_promoçao['id_produto']; ?>"  
-        >
-            <img src="imagens/exclusivo/<?php echo $row_promoçao['imagem_produto']; ?>" class="card-img-top img-fluid" alt="Promoção 1">
-            <a
-                         href="Produto_detalhe.php?id_produto=<?php echo $row_promoçao['id_produto']; ?>">                       
-                    </a>  
-            <h6 class="card-title mb-1"><?php echo $row_promoçao['nome_produto']; ?></h6>
-          <p class="card-text fw-bold">
-<?php echo $row_promoçao['valor_produto'];  ?></p>
-          </div> 
-           <?php }while($i <= 2 && $row_promoçao=$lista_promoçao->fetch_assoc()); ?>   <!--Fecha estrut. de repetição  -->
-
-         <!-- direita card  -->
-         <?php $i = 1; 
-         do{ $i++; ?>
-        <div class="col-6 col-md-3 mb-3 " >
-          <div class="card text-center mb-3">
-            <a   
-           href="Produto_detalhe.php?id_produto=<?php echo $row_promoçao['id_produto']; ?>"  
-        >
-            <img src="imagens/exclusivo/<?php echo $row_promoçao['imagem_produto'];?>" class="card-img-top img-fluid" alt="Promoção 3">
-             <a
-                         href="Produto_detalhe.php?id_produto=<?php echo $row_promoçao['id_produto']; ?>">                       
-                    </a>  
-            <h6 class="card-title mb-1"><?php echo $row_promoçao['nome_produto'];  ?></h6>
-          <p class="card-text fw-bold">
-           <?php echo number_format($row_promoçao['valor_produto'],2,',','.'); ?></p>
+        <?php 
+        // ----- Próximos 4 cards pequenos -----
+        $contador = 0;
+        while($contador < 4 && $row = $lista_promoçao->fetch_assoc()) {
+        ?>
+        <div class="col-6 col-md-3 mb-3">
+          <div class="card text-center">
+            <a href="Produto_detalhe.php?id_produto=<?= $row['id_produto']; ?>">
+              <img src="imagens/exclusivo/<?= $row['imagem_produto']; ?>" 
+                   class="card-img-top img-fluid">
+            </a>
+            <h6 class="card-title mb-1"><?= $row['nome_produto']; ?></h6>
+            <p class="card-text fw-bold">
+              <?= number_format($row['valor_produto'],2,',','.'); ?>
+            </p>
           </div>
-          <?php }while($i <= 2 && $row_promoçao=$lista_promoçao->fetch_assoc()); ?>           
+        </div>
+        <?php $contador++; } ?>
 
       </div>
-    </div> 
+    </div>
   </div>
 </div>
 
-<script           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-           crossorigin="anonymous"
-        ></script>
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
 
 <?php mysqli_free_result($lista_promoçao); ?>
